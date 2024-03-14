569ce05ca3f405ea1e8ef75073dd2a2cbef3188a
Jadon <jadonzufall@gmail.com>
2024-03-14 12:09:28 -0400

    updated edge.py

--- src/obj/edge.py
+++ src/obj/edge.py
@@ -1,7 +1,15 @@
+from typing import Optional
 from src.obj.node import Node
 
 class Edge:
-    def __init__(self, src: "Node", dst: "Node") -> None:
+    """ Represents a connection between two nodes. """
+    def __init__(self, src: Optional["Node"], dst: Optional["Node"], weight: float=1.0) -> None:
+        """ Construct a new Edge instance. """
         self._src: "Node" = src
         self._dst: "Node" = dst
+        self._weight: float = weight
+    
+    def reverse(self) -> "Edge":
+        """ Make the edge go in the reverse direction. """
+        return Edge(src=self._dst, dst=self._src, weight=self._weight)
 
