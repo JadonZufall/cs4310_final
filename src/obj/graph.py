d898bbe3a4467170f8339780fe1dc5e9749582b5
czehner1027 <ckm3320@wmich.edu>
2024-03-26 03:21:20 -0400

    Continured Graph and Node. Node and Edge files were consolodated to one file

--- src/obj/graph.py
+++ src/obj/graph.py
@@ -1,4 +1,11 @@
 from src.obj.node import Node
+from typing import Optional
+import random
+
+XMIN = 0
+XMAX = 100
+YMIN = 0
+YMAX = 100
 
 class Graph:
     def __init__(self, nodes: list[Node]=[]) -> None:
@@ -7,7 +14,15 @@ class Graph:
     def contains(self, instance: Node) -> bool:
         return instance in self._nodes
     
-    def create_edge(self, node1: Node, node2: Node, weighted: bool, directed: bool) -> None:
+    def add_node(self, node: Optional[Node] = None):
+        # Generate a random new node
+        if node is None:
+            self._nodes.append(Node(None, random.randrange(XMIN, XMAX), random.randrange(YMIN, YMAX)))
+        else: 
+            self._nodes.append(node)
+
+
+    def add_edge(self, node1: Node, node2: Node, weighted: bool, directed: bool) -> None:
         if self.contains(node1) and self.contains(node2):
             node1.add_edge(node2, weighted)
             if not directed:
@@ -15,3 +30,17 @@ class Graph:
     
     def get_nodes(self) -> list[Node]:
         return self._nodes
+    
+    def get_random_node(self) -> Node:
+        return random.choice(self._nodes) if len(self._nodes) != 0 else None
+    
+    def print(self):
+        for node in self._nodes:
+            edges = node.get_edges()
+            for edge in edges:
+                print(f"{node.get_value()} {edge.get_destination().get_value()}")
+    
+    def merge(self, g2: 'Graph', n1: Node, n2: Node, weighted: bool, directed: bool):
+        for node in g2.get_nodes():
+            self.add_node(node)
+        self.add_edge(n1, n2, weighted, directed)
--- src/obj/node.py
+++ src/obj/node.py
@@ -1,22 +1,54 @@
 from typing import Optional
-from edge import Edge
-from node import Node
+from math import sqrt as SQRT
+from math import pow as POW
+
+class Edge:
+    """ Represents a connection between two nodes. """
+    def __init__(self, src: 'Node', dst: 'Node', weighted: bool) -> None:
+        """ Construct a new Edge instance. """
+        self._src: Node = src
+        self._dst: Node = dst
+
+        """ Calculate weight (1 for unweighted graphs)"""
+        src_x, src_y = src.get_coordinates()
+        dst_x, dst_y = src.get_coordinates()
+        self._weight: float = 1.0 if not weighted else SQRT(POW(src_x - dst_x, 2) + POW(src_y - dst_y, 2))
+    
+    def contains(self, instance: 'Node') -> bool:
+        return instance is self._src or instance is self._dst
+    
+    def reverse(self) -> "Edge":
+        """ Make the edge go in the reverse direction. """
+        return Edge(src=self._dst, dst=self._src, weight=self._weight)
+    
+    def get_source(self) -> 'Node':
+        return self._src
+    
+    def get_destination(self) -> 'Node':
+        return self._dst
+    
+    def get_weight(self) -> float:
+        return self._weight
 
 class Node:
     def __init__(self, value: Optional[str]=None, x: int = 0, y: int = 0) -> None:
         self._value: Optional[str] = value
-        self._edges: list[Edge] = None
+        self._edges: list[Edge] = []
         self._x: int = x
         self._y: int = y
-
+    
     def get_coordinates(self) -> list[int]:
         return self._x, self._y
 
-    def add_edge(self, dst: Node, weighted: bool) -> None:
+    def get_value(self) -> str:
+        return self._value
+
+    def add_edge(self, dst: 'Node', weighted: bool) -> None:
         self._edges.append(Edge(self, dst, weighted))
 
+    
     def get_edges(self) -> list[Edge]:
         return self._edges
     
-    def get_neighbors(self) -> list[Node]:
+    def get_neighbors(self) -> list['Node']:
         return [edge.get_destination() for edge in self._edges]
