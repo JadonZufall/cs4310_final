cd20c2f1fc3b7cba8f9f862c62d2d1622a3e7730
czehner1027 <ckm3320@wmich.edu>
2024-03-26 03:22:00 -0400

    Began implementing graph generators

---
+++ src/tests/graph_generator.py
@@ -0,0 +1,17 @@
+from src.obj.graph import Graph
+import src.obj.graph as graph
+from src.obj.node import Node
+import random
+import copy
+
+MAX_NODES = 31
+
+def generate_tree_graph(num_nodes: int = random.randrange(0, MAX_NODES), weighted: bool = True, directed: bool = False) -> Graph:
+    nodes = [Node(str(x), random.randrange(graph.XMIN, graph.XMAX), random.randrange(graph.YMIN, graph.YMAX)) for x in range(0, num_nodes)]
+    graphs = [Graph([nodes[i]]) for i in range(0, num_nodes)]
+    while len(graphs) != 1:
+        g1 = random.choice(graphs)
+        graphs.remove(g1)
+        g2 = random.choice(graphs)
+        g2.merge(g1, g2.get_random_node(), g1.get_random_node(), weighted, directed)
+    return graphs[0]
