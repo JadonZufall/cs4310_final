b008fc5dc6ae9a5c576fc4dc54d72694fd5c343a
czehner1027 <ckm3320@wmich.edu>
2024-04-17 03:12:52 -0400

    Dijkstra's now returns the list of edges which were added in order and which edges were considered

--- src/pathfinders/dijkstra.py
+++ src/pathfinders/dijkstra.py
@@ -13,25 +13,46 @@ def dijkstra(graph: Graph, start: Node):
     distances = [float('inf')] * graph.get_num_nodes()
     distances[start._index] = 0
 
+    # Keep track of the order in which edges are added and considered
+    added_edges = []
+    considered_edges = []
+
     # Repeatedly pop from priority queue
     while pq:
+        # Get the next edge in the pqueue
         current_distance, current_edge = heapq.heappop(pq)
 
+        # Get the 2 nodes of the edge
         previous = current_edge._src
         current = current_edge._dst
 
+        # Only consider nodes which have not previously been reached
         if (prev[current._index] == None):
-            if previous != None:
+            # New edge has been added
+            added_edges.append(current_edge)
+
+            # Cannot set the start of the parent
+            if current is not start:
                 prev[current._index] = previous._index
 
+            # Get all of the adjacent edges
             adjacent_edges = current.get_edges()
 
+            # Keep track of which edges were considered
+            considered = []
+
+            # Check every adjacent edge
             for adjacent_edge in adjacent_edges:
+                # Get the weight and the node
                 weight = adjacent_edge._weight
                 prospect = adjacent_edge._dst
 
+                # If the distance is less than the current distance for that node, keep track of it
                 if distances[prospect._index] > current_distance + weight:
+                    considered.append(adjacent_edge)
                     distances[prospect._index] = current_distance + weight
                     heapq.heappush(pq, (distances[prospect._index], adjacent_edge))
 
-    return distances, prev
+            considered_edges.append(considered)
+
+    return distances, prev, added_edges, considered_edges
